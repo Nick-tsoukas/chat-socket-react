28991628df238d2dc996ee8b53fac3e3b8d1462d
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-12-01 12:18:54 -0600

    ...

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
 const Chat = () => {
+    const logger = (event) => {
+        event.preventDefault();
+        console.log("you just clicked the button hello how are you doing today")
+    }
     return(
         <div className="chat_box">
-            <h2>Chats </h2>
+            <div className="chat_header">
+                <h2>Chats</h2>
+            </div>
+            <div className="chat_messages">
+                <p>This is where the chats are</p>
+                <p>This is where the chats are</p>
+                <p>This is where the chats are</p>
+                <p>This is where the chats are</p>
+                <p>This is where the chats are</p>
+            </div>
+            <div className="chat_input">
+            <form onSubmit={logger}>
+                <input type="text" name="name" />
+                <input type="submit" value="Submit" />
+            </form>
+            </div>
         </div>
     )
 }
